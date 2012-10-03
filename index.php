75033da9d66bd92c8eec163617960341fe592637
Justin Tseko <justintseko@gmail.com>
2012-10-03 18:38:08 -0400

    Test

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+
+<html>
+<head>
+<title>About Justin Tseko</title>
+<style type="text/css">
+font-family {DejaVu Sans Mono, Courier Sans, sans-serif;} color {white;}
+background-color {#101025}
+a {font-weight: bold; color: #aaa}
+</style>
+</head>
+<body style="font-family:DejaVu Sans Mono, Courier Sans, sans-serif; color:white; background-color:#101025">
+
+<h1>About Justin</h1>
+
+<p>Hi Everyone, I'm Justin! I'm 20 and I live in Porter Square in Cambridge. I hope to pursue web development/design as a career, so I really look forward to this class. For me, web development represents the perfect blend of nerdy programming and artistic design, and being able to have a career creating web pages is an exciting prospect!</p>
+<p>I am using asmallorange's small hosting plan, and they also host my domain name. For code editing I use both Notepad++ and Dreamweaver (I know, I know, but it's really helpful sometimes).</p>
+<p>And here's a link to my Git repo: <a href="https://github.com/justintseko/dwa">https://github.com/justintseko/dwa</a></p>
+</body>
+</html>
