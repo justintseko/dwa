e4125d402502d4530d42baf85cb83be2905fb880
Justin Tseko <justintseko@gmail.com>
2012-11-01 23:29:00 -0400

    10/31 class and added features

--- p2.jte75.com/views/v_posts_users.php
+++ p2.jte75.com/views/v_posts_users.php
@@ -1,15 +1,14 @@
 <form method='POST' action='/posts/p_follow'>
 		
-	<? foreach($users as $user): ?>
+	<? foreach($users as $key => $user): ?>
 	
-		<!-- Print this user's name -->
-		<?=$user['first_name']?> <?=$user['last_name']?>
+		<a href='/posts/viewuser/<?=$user['user_id']?>'><?=$user['first_name']?> <?=$user['last_name']?> </a>
 		
-		<!-- If there exists a connection with this user, show a unfollow link -->
+		<? $user_id = $user['user_id'] ?>
+		
 		<? if(isset($connections[$user['user_id']])): ?>
 			<a href='/posts/unfollow/<?=$user['user_id']?>'>Unfollow</a>
 		
-		<!-- Otherwise, show the follow link -->
 		<? else: ?>
 			<a href='/posts/follow/<?=$user['user_id']?>'>Follow</a>
 		<? endif; ?>
