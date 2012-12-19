c16e0abcb8b79299596539c23881d6e28a3a4409
Justin Tseko <justintseko@gmail.com>
2012-12-18 23:34:07 -0500

    Fixing Display Issue

--- p4.jte75.com/views/v_news_viewnews.php
+++ p4.jte75.com/views/v_news_viewnews.php
@@ -10,38 +10,23 @@ function OnLoad() {
   // Add feeds.
   feedControl.addFeed("<?=$news['feed_1_url']?>", "<?=$news['feed_1_name']?>");
 
-  // Draw it.
-  feedControl.draw(document.getElementById("content1"));
-
   feedControl.addFeed("<?=$news['feed_2_url']?>", "<?=$news['feed_2_name']?>");
-
-  feedControl.draw(document.getElementById("content2"));
 
   feedControl.addFeed("<?=$news['feed_3_url']?>", "<?=$news['feed_3_name']?>");
 
-  feedControl.draw(document.getElementById("content3"));
-
   feedControl.addFeed("<?=$news['feed_4_url']?>", "<?=$news['feed_4_name']?>");
- 
-  feedControl.draw(document.getElementById("content4"));
 
   feedControl.addFeed("<?=$news['feed_5_url']?>", "<?=$news['feed_5_name']?>");
- 
-  feedControl.draw(document.getElementById("content5"));
 
   feedControl.addFeed("<?=$news['feed_6_url']?>", "<?=$news['feed_6_name']?>");
   
-  feedControl.draw(document.getElementById("content6"));
+    // Draw it.
+  feedControl.draw(document.getElementById("content1"));
 }
 
 google.setOnLoadCallback(OnLoad);
 </script>
 	
 <div id="content1"></div>
-<div id="content2"></div>
-<div id="content3"></div>
-<div id="content4"></div>
-<div id="content5"></div>
-<div id="content6"></div>
 	
 <? endforeach; ?>
